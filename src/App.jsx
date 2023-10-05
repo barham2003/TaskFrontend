00739f4c332d549d8d214a5d788b763677930a80
Baran <baranali6688@gmail.com>
2023-10-05 09:49:14 +0300

    Fixing Bugs in Local

--- src/App.jsx
+++ src/App.jsx
@@ -16,25 +16,25 @@ function App() {
   const [eMessage, setEMessage] = useState(false)
   const [messageBody, setMessageBody] = useState("")
   const [activeGroup, setActiveGroup] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
 
   // Fetching Data
   const fetchData = async () => {
-    // const fetchData = async () => {
-    const resGroup = await fetch("https://mytasksapi.onrender.com/groups")
+    const resGroup = await fetch("/api/groups")
     if (resGroup.ok) {
       const json = await resGroup.json()
       setActiveGroup("main")
       dispatchG({ type: "FETCH", payload: json })
     }
 
-    const resTask = await fetch("https://mytasksapi.onrender.com/tasks")
+    const resTask = await fetch("/api/tasks")
     if (resTask.ok) {
       const json = await resTask.json()
       // Set the data in UseReducer
       setInitTasks(json)
       dispatch({ type: "FETCH", payload: json })
     }
-
+    setIsLoading(false)
   }
 
 
@@ -98,7 +98,6 @@ function App() {
         return { groups: [...state.groups, action.payload] }
 
       case "REMOVE":
-        console.log("FROM REDUCER: ", action.payload)
         if (action.payload.tasks.length > 0) return state
         const removeGroup = state.groups.filter(item => item._id !== action.payload._id)
         return { groups: removeGroup }
@@ -115,8 +114,14 @@ function App() {
 
   // Creating Group
   const postGroup = async () => {
-    dispatchG({ type: "ADD", payload: { name: groupName, tasks: [], createdAt: Date.now() } })
-    const res = await fetch("https://mytasksapi.onrender.com/groups", {
+    setIsLoading(true)
+    const groupArray = []
+    groups.forEach(e => groupArray.push(e.name))
+
+    if (groupName && !groupArray.includes(groupName)) {
+      dispatchG({ type: "ADD", payload: { name: groupName, tasks: [], createdAt: Date.now() } })
+    }
+    const res = await fetch("/api/groups", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -124,14 +129,18 @@ function App() {
     })
 
 
+
     if (res.ok) {
-      fetchData()
+      setActiveGroup(groupName)
+      dispatch({ type: 'FETCH', payload: [] })
+      setIsLoading(false)
     }
 
     if (!res.ok) {
       const json = await res.json()
       setEMessage(true)
       setMessageBody(json.message)
+      setIsLoading(false)
       return
     }
   }
@@ -141,7 +150,8 @@ function App() {
   //Deleting Group
   const DeleteGroup = async (group) => {
     dispatchG({ type: "REMOVE", payload: group })
-    const res = await fetch("https://mytasksapi.onrender.com/groups/" + group._id, {
+    setGroup("main")
+    const res = await fetch("/api/groups/" + group.name, {
       method: "DELETE",
     })
 
@@ -154,6 +164,7 @@ function App() {
 
     if (res.ok) {
       setEMessage(false)
+      fetchData()
     }
 
   }
@@ -168,6 +179,7 @@ function App() {
   const setGroup = (name) => {
     if (name === "main") return dispatch({ type: "MAIN" })
     dispatch({ type: "FILTER", payload: name })
+
   }
 
 
@@ -181,11 +193,17 @@ function App() {
 
       <section className='sidebar bg-slate-800 shadow-sm text-white border-r border-gray-700 flex flex-col justify-center gap-y-8 items-center'>
 
-        <div className={`w-72 h-20 bg-slate-900 rounded-lg flex justify-center items-center flex-col pt-2 hover:bg-slate-950 active:border-2 active:border-indigo-800 ${activeGroup === "main" ? "bg-slate-950 border-indigo-800 border-2" : ""}`} onClick={() => setGroup("main")} >
-          <h1 className="text-xl" >Main</h1>
 
-        </div>
-        {groups && groups.map(group => {
+        {isLoading ? <div className="flex justify-center items-center animate-spin col-start-2 text-white">
+          <svg fill="white" className=" stroke-indigo-800 stroke-2" height="65" viewBox="0 0 20 20" width="65" xmlns="http://www.w3.org/2000/svg"><path d="M10 3.5C6.41015 3.5 3.5 6.41015 3.5 10C3.5 10.4142 3.16421 10.75 2.75 10.75C2.33579 10.75 2 10.4142 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 14.4183 14.4183 18 10 18C9.58579 18 9.25 17.6642 9.25 17.25C9.25 16.8358 9.58579 16.5 10 16.5C13.5899 16.5 16.5 13.5899 16.5 10C16.5 6.41015 13.5899 3.5 10 3.5Z" fill="#212121" /></svg>
+        </div> :
+          <div className={`w-72 h-20 bg-slate-900 rounded-lg flex justify-center items-center flex-col pt-2 hover:bg-slate-950 active:border-2 active:border-indigo-800 ${activeGroup === "main" ? "bg-slate-950 border-indigo-800 border-2" : ""}`} onClick={() => setGroup("main")} >
+            <h1 className="text-xl" >Main</h1>
+          </div>}
+
+
+
+        {!isLoading && groupContainer.groups && groupContainer.groups.map(group => {
           return (
             <div className={`w-72 h-20 bg-slate-900 rounded-lg flex justify-center items-center flex-col pt-2 hover:bg-slate-950 active:border-2 active:border-indigo-800 ${activeGroup === group.name ? "bg-slate-950 border-indigo-800 border-2" : ""} `} onClick={() => setGroup(group.name)}>
               <h1 className="text-xl" >{group.name}</h1>
@@ -194,16 +212,23 @@ function App() {
             </div>
           )
         })}
-        <div className="flex flex-col gap-y-2">
+
+        {!isLoading && <div className="flex flex-col gap-y-2">
           <input type="text" id="title" value={groupName} onChange={(e) => setGroupName(e.target.value)} className="rounded-sm font-bold pl-1 text-gray-900" />
           <button className=" text-xs bg-indigo-700 text-white p-2 rounded-3xl px-3 font-bold hover:bg-indigo-800 hover:shadow-lg active:bg-white  active:text-indigo-700 transition-colors delay-50" onClick={() => { postGroup() }}>+Add New Group</button>
-        </div>
+        </div>}
+
 
       </section>
 
 
       <main className="main gap-x-8 p-14 mx-20">
-        <MainPage tasks={taskContainer.tasks} dispatch={dispatch} activeGroup={activeGroup} />
+        {isLoading ? <div className="flex justify-center items-center animate-spin col-start-2 text-white">
+          <svg fill="white" className=" stroke-indigo-800 stroke-2" height="65" viewBox="0 0 20 20" width="65" xmlns="http://www.w3.org/2000/svg"><path d="M10 3.5C6.41015 3.5 3.5 6.41015 3.5 10C3.5 10.4142 3.16421 10.75 2.75 10.75C2.33579 10.75 2 10.4142 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 14.4183 14.4183 18 10 18C9.58579 18 9.25 17.6642 9.25 17.25C9.25 16.8358 9.58579 16.5 10 16.5C13.5899 16.5 16.5 13.5899 16.5 10C16.5 6.41015 13.5899 3.5 10 3.5Z" fill="#212121" /></svg>
+        </div> :
+          <MainPage tasks={taskContainer.tasks} dispatch={dispatch} activeGroup={activeGroup} />
+        }
+
       </main>
     </div>
   )
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -11,18 +11,17 @@ function MainPage({ activeGroup, tasks, dispatch }) {
 
     const deleteTask = async (id) => {
         dispatch({ type: "REMOVE", payload: id })
-        const res = await fetch("https://mytasksapi.onrender.com/tasks/" + id, {
+        const res = await fetch("/api/tasks/" + id, {
             method: "DELETE"
         })
         if (res.ok) {
             const json = await res.json()
-            console.log(json)
         }
     }
 
     const changeTask = async (id, state) => {
         dispatch({ type: "CHANGE", payload: { id, state } })
-        const res = await fetch(`https://mytasksapi.onrender.com/tasks/${id}/${state}`,
+        const res = await fetch(`/api/tasks/${id}/${state}`,
             { method: "PATCH" }
         )
         if (res.ok) {
