d227e5685bd488187d59fa670ec81c4e10ab3936
Baran <baranali6688@gmail.com>
2023-10-14 16:20:36 +0300

    Adding Animations!!

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Navbar from './components/navbar';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useReducer } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
 
 
 
@@ -12,6 +14,8 @@ import { useReducer } from 'react';
 function App() {
   // States
   const [groupName, setGroupName] = useState("")
+  const [showMainPage, setShowMainPage] = useState(true);
+
   const [initTasks, setInitTasks] = useState([])
   const [eMessage, setEMessage] = useState(false)
   const [messageBody, setMessageBody] = useState("")
@@ -38,7 +42,6 @@ function App() {
   }
 
 
-
   useEffect(() => {
     fetchData()
   }, [])
@@ -52,13 +55,15 @@ function App() {
   const taskReducer = (state, action) => {
     switch (action.type) {
       case 'FETCH':
+        setTimeout(() => { setShowMainPage(true) }, 500)
+
         return { tasks: action.payload };
       case 'REMOVE':
         const removeTask = state.tasks.filter(item => item._id !== action.payload)
         return { tasks: removeTask };
       case 'ADD':
         fetchData()
-        return { tasks: [...state.tasks, action.payload] };
+        return { tasks: [action.payload, ...state.tasks] };
       case 'CHANGE':
         const updatedTasks = state.tasks
           .map(item => {
@@ -68,6 +73,7 @@ function App() {
         return { tasks: updatedTasks };
 
       case 'FILTER':
+        setTimeout(() => { setShowMainPage(!showMainPage) }, 500)
         const filteredTasks = [...initTasks].filter(item => item.group.name === action.payload)
         setActiveGroup(action.payload)
         return { tasks: filteredTasks }
@@ -177,9 +183,9 @@ function App() {
 
   // Setting Group
   const setGroup = (name) => {
+    setShowMainPage(!showMainPage)
     if (name === "main") return dispatch({ type: "MAIN" })
     dispatch({ type: "FILTER", payload: name })
-
   }
 
 
@@ -188,6 +194,7 @@ function App() {
 
 
   return (
+
     <div className="layout bg-slate-900 h-screen selection:bg-slate-600 relative">
       <Navbar dispatch={dispatch} eMessage={eMessage} changeMessage={changeMessage} messageBody={messageBody} />
 
@@ -221,16 +228,22 @@ function App() {
 
       </section>
 
+      <CSSTransition
+        timeout={500}
+        classNames="component"
+        unmountOnExit
+        in={showMainPage}
+      >
+        <main className="main gap-x-8 p-14 mx-20">
+          {isLoading ? <div className="flex justify-center items-center animate-spin col-start-2 text-white">
+            <svg fill="white" className=" stroke-indigo-800 stroke-2" height="65" viewBox="0 0 20 20" width="65" xmlns="http://www.w3.org/2000/svg"><path d="M10 3.5C6.41015 3.5 3.5 6.41015 3.5 10C3.5 10.4142 3.16421 10.75 2.75 10.75C2.33579 10.75 2 10.4142 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 14.4183 14.4183 18 10 18C9.58579 18 9.25 17.6642 9.25 17.25C9.25 16.8358 9.58579 16.5 10 16.5C13.5899 16.5 16.5 13.5899 16.5 10C16.5 6.41015 13.5899 3.5 10 3.5Z" fill="#212121" /></svg>
+          </div> :
+            <MainPage tasks={taskContainer.tasks} dispatch={dispatch} activeGroup={activeGroup} />
+          }
 
-      <main className="main gap-x-8 p-14 mx-20">
-        {isLoading ? <div className="flex justify-center items-center animate-spin col-start-2 text-white">
-          <svg fill="white" className=" stroke-indigo-800 stroke-2" height="65" viewBox="0 0 20 20" width="65" xmlns="http://www.w3.org/2000/svg"><path d="M10 3.5C6.41015 3.5 3.5 6.41015 3.5 10C3.5 10.4142 3.16421 10.75 2.75 10.75C2.33579 10.75 2 10.4142 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 14.4183 14.4183 18 10 18C9.58579 18 9.25 17.6642 9.25 17.25C9.25 16.8358 9.58579 16.5 10 16.5C13.5899 16.5 16.5 13.5899 16.5 10C16.5 6.41015 13.5899 3.5 10 3.5Z" fill="#212121" /></svg>
-        </div> :
-          <MainPage tasks={taskContainer.tasks} dispatch={dispatch} activeGroup={activeGroup} />
-        }
-
-      </main>
-    </div>
+        </main>
+      </CSSTransition>
+    </div >
   )
 }
 
