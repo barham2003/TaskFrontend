ac47612b07a82611f468fd6b91911b8af36fd811
Baran <baranali6688@gmail.com>
2023-11-17 16:04:33 +0300

    Finished Other Things!

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function Task({ task, onDelete, onChange }) {
 	return (
 		<div className="one-task">
 			<h1 className="one-task-head">{task.title}</h1>
-			<p className="task-description">{task.description}</p>
+			<p className="task-description">{task.body}</p>
 			<div className="flex gap-x-2 text-xs">
 				<button
 					className="btn round-btn "
@@ -23,7 +23,9 @@ function Task({ task, onDelete, onChange }) {
 					Delete
 				</button>
 			</div>
-			<span className="time-stamp">{task.date}</span>
+			<span className="time-stamp">
+				{new Date(task.createdAt).toLocaleDateString()}
+			</span>
 		</div>
 	)
 }
@@ -55,63 +57,20 @@ function TasksContainer({ tasks, state, onDelete, onChange }) {
 	)
 }
 
-const tempTasks = [
-	{
-		id: 1,
-		title: "Task 1",
-		description: "Complete the project proposal",
-		date: "2023-11-17",
-		state: "todo",
-		category: "Work",
-	},
-	{
-		id: 2,
-		title: "Task 2",
-		description: "Research and gather information for the report",
-		date: "2023-11-18",
-		state: "doing",
-		category: "Study",
-	},
-	{
-		id: 3,
-		title: "Task 3",
-		description: "Review and revise the code",
-		date: "2023-11-19",
-		state: "doing",
-		category: "Work",
-	},
-	{
-		id: 4,
-		title: "Task 4",
-		description: "Prepare presentation slides",
-		date: "2023-11-20",
-		state: "done",
-		category: "Work",
-	},
-	{
-		id: 5,
-		title: "Task 5",
-		description: "Submit final deliverables",
-		date: "2023-11-21",
-		state: "done",
-		category: "Personal",
-	},
-]
-
 function NewModal({ onAdd, onClose }) {
 	const [title, setTitle] = useState("")
 	const [desc, setDesc] = useState("")
-	const [category, setCategory] = useState("")
+	const [group, setCategory] = useState("")
 	const [state, setState] = useState("todo")
 
 	function handleOnSubmit() {
-		const newTask = { title, desc, category, state, id: +Date.now() }
-		if (!title || !desc || !category || !state) return console.log("Error")
+		const newTask = { title, body: desc, group, state }
+		if (!title || !desc || !group || !state) return console.log("Error")
 		onAdd(newTask)
 	}
 	return (
 		<div className="absolute inset-0 bg-new ">
-			<div className="modal md:h-2/3 md:w-2/3 ">
+			<div className="modal">
 				<div className="flex flex-col">
 					<h1 className="text-2xl mb-4">New Task</h1>
 					<label>Title</label>
@@ -134,14 +93,14 @@ function NewModal({ onAdd, onClose }) {
 					<input
 						type="text"
 						className="input"
-						value={category}
+						value={group}
 						onChange={e => setCategory(e.target.value)}
 					/>
 
 					<label>State</label>
 					<select
 						name="state"
-						className="bg-slate-700"
+						className="select-input"
 						value={state}
 						onChange={e => setState(e.target.value)}>
 						<option value="todo">Todo</option>
@@ -168,37 +127,73 @@ function NewModal({ onAdd, onClose }) {
 function AuthModal({ onLogin }) {
 	const [username, setUsername] = useState("")
 	const [password, setPassword] = useState("")
+
+	const [newUsername, setNewUsername] = useState("")
+	const [newName, setNewName] = useState("")
+	const [newPassword, setNewPassword] = useState("")
+	const [confirmPassword, setCofirmPassword] = useState("")
 	const [error, setError] = useState(null)
+
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	}
+
 	async function sign() {
 		if (!username || !password) return setError("Fill All Fields")
+		const user = { username: username, password: password }
 		const res = await fetch("/api/auth/login", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ username: username, password: password }),
+			...options,
+			body: JSON.stringify(user),
 		})
 
 		const data = await res.json()
 		if (res.ok) {
 			onLogin(data)
-			localStorage.setItem("data", JSON.stringify(data))
 		}
 		if (!res.ok) {
 			setError(data.message)
-			// console.log(data.message)
 		}
-		// console.log(res, " --- ", data)
+	}
+
+	async function signup() {
+		if (!newName || !newPassword || !confirmPassword || !newUsername)
+			return setError("Please fill or fields")
+		if (newPassword !== confirmPassword)
+			return setError("Password and confirm password are not the same")
+
+		const user = {
+			name: newName,
+			password: newPassword,
+			username: newUsername,
+			passwordConfirm: confirmPassword,
+		}
+
+		const res = await fetch("/api/auth/signup", {
+			...options,
+			body: JSON.stringify(user),
+		})
+
+		const data = await res.json()
+
+		if (res.ok) {
+			onLogin(data)
+		}
+
+		if (!res.ok) {
+			setError(data.message)
+		}
 	}
 
 	return (
 		<div className="absolute inset-0 bg-new ">
-			<div className="modal">
+			<div className="newmodal">
 				<div className="flex flex-col">
 					<h1 htmlFor="sign" className="text-2xl mb-4">
 						Sign
 					</h1>
-					{error && <div className="error">{error}</div>}
 					<label htmlFor="username">Username</label>
 					<input
 						className="input"
@@ -218,21 +213,54 @@ function AuthModal({ onLogin }) {
 						<button className="btn round-btn text-lg" onClick={() => sign()}>
 							Sign in
 						</button>
+						{error && (
+							<div
+								className="error absolute top-0 left-0"
+								onClick={() => setError(null)}>
+								{error}
+							</div>
+						)}
 					</div>
 				</div>
-				<span>OR</span>
+				<div className="md:w-14"></div>
 				<div className="flex flex-col">
 					<h1 htmlFor="regester" className="text-2xl mb-4">
 						Register
 					</h1>
+					<label htmlFor="name">Name</label>
+					<input
+						type="text"
+						value={newName}
+						onChange={e => setNewName(e.target.value)}
+						className="input"
+					/>
 					<label htmlFor="username">Username</label>
-					<input type="text" className="input" />
+					<input
+						type="text"
+						value={newUsername}
+						onChange={e => setNewUsername(e.target.value)}
+						className="input"
+					/>
 					<label htmlFor="passowrd">Password</label>
-					<input type="password" className="input" />
+					<input
+						type="password"
+						value={newPassword}
+						onChange={e => {
+							setNewPassword(e.target.value)
+						}}
+						className="input"
+					/>
 					<label htmlFor="confirmPass">Confirm Password</label>
-					<input type="passowrd" className="input" />
+					<input
+						type="password"
+						value={confirmPassword}
+						onChange={e => setCofirmPassword(e.target.value)}
+						className="input"
+					/>
 					<div className="my-4">
-						<button className="btn round-btn text-lg">Register</button>
+						<button className="btn round-btn text-lg" onClick={() => signup()}>
+							Register
+						</button>
 					</div>
 				</div>
 			</div>
@@ -242,82 +270,82 @@ function AuthModal({ onLogin }) {
 
 function App() {
 	const [showSidebar, setShowSidebar] = useState(false)
-	const [initTasks, setInitTasks] = useState(tempTasks)
+	const [initTasks, setInitTasks] = useState([])
 	const [filteredTasks, setFilteredTasks] = useState([...initTasks])
 	const [distinctCategories, setDistinctCategories] = useState([])
-	const [activeCategory, setCategory] = useState("Main")
+	const [activeGroup, setGroup] = useState("Main")
 	const [isAuth, setIsAuth] = useState(false)
 	const [isNewOpen, setIsNewOpen] = useState(false)
-	// const [user, setUser] = useState(null)
-	// const [token, setToken] = useState(null)
 	const [authData, setAuthData] = useState(null)
-
 	const token = authData?.token
 	const user = authData?.user
+	const headers = new Headers({
+		"Content-Type": "application/json",
+		Authorization: `Bearer ${token}`,
+	})
 
 	useEffect(
 		function () {
-			setDistinctCategories([...new Set(initTasks.map(task => task.category))])
-			if (activeCategory === "Main") return setFilteredTasks([...initTasks])
-			setFilteredTasks(
-				initTasks.filter(task => task.category === activeCategory)
-			)
+			setDistinctCategories([...new Set(initTasks.map(task => task.group))])
+			if (activeGroup === "Main") return setFilteredTasks([...initTasks])
+			setFilteredTasks(initTasks.filter(task => task.group === activeGroup))
 		},
-		[initTasks, activeCategory]
+		[initTasks, activeGroup]
 	)
 
-	function handleSetCategory(category) {
-		setCategory(category)
+	function handleSetGroup(group) {
+		setGroup(group)
 	}
 
-	function handleOnDelete(id) {
+	async function handleOnDelete(id) {
+		await fetch(`/api/tasks/${id}`, { method: "DELETE", headers })
 		setInitTasks([...initTasks.filter(tasks => tasks.id !== id)])
 	}
 
-	function handleOnChange(id, state) {
+	async function handleOnChange(id, state) {
 		setInitTasks([
 			...initTasks.map(task => (task.id === id ? { ...task, state } : task)),
 		])
+		await fetch(`/api/tasks/${id}`, {
+			method: "PATCH",
+			headers,
+			body: JSON.stringify({ state }),
+		})
 	}
 
 	function setModal() {
 		setIsNewOpen(value => !value)
 	}
 
-	function handleOnAdd(newTask) {
-		setInitTasks([newTask, ...initTasks])
+	async function handleOnAdd(newTask) {
+		const res = await fetch("/api/tasks", {
+			method: "POST",
+			headers,
+			body: JSON.stringify(newTask),
+		})
+		const { data } = await res.json()
+		if (res.ok) {
+			setInitTasks([data, ...initTasks])
+		}
 		setIsNewOpen(value => !value)
 	}
 	useEffect(() => {
-		const initData = JSON.parse(localStorage.getItem("data"))
+		const initData = JSON.parse(localStorage?.getItem("data"))
 
 		const fetchData = async () => {
-			const headers = new Headers({
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${token}`,
-			})
-
-			const requestOptions = {
-				method: "GET",
-				headers,
-			}
-
-			const res = await fetch("/api/tasks", requestOptions)
+			const res = await fetch("/api/tasks", { method: "GET", headers })
 			const data = await res.json()
-			console.log(data)
+			setInitTasks(data.data)
 		}
 
-		if (initData) {
-			setAuthData(initData)
-		}
+		if (initData) setAuthData(initData)
 		if (token) fetchData()
 	}, [token])
 
 	function handleLogin(data) {
-		// setUser(data.user)
-		// setToken(data.token)
 		setIsAuth(true)
 		setAuthData(data)
+		localStorage.setItem("data", JSON.stringify(data))
 	}
 
 	function handleLogout() {
@@ -358,7 +386,7 @@ function App() {
 						<button className="btn round-btn" onClick={setModal}>
 							+Add New Task
 						</button>
-						<span className="person" onClick={handleLogout}>
+						<span className="person cursor-pointer" onClick={handleLogout}>
 							{user?.name}
 						</span>
 					</div>
@@ -369,19 +397,24 @@ function App() {
 						showSidebar ? " md:col-span-1 col-span-5 md:flex" : "hidden"
 					}  `}>
 					<div className="stats">
-						<div className=""> All Boards (8)</div>
+						<div className="nums">
+							Tasks Num: {initTasks.length} {"\n"}
+							Categories Num: {distinctCategories.length}
+						</div>
 					</div>
 					<div className="groups">
-						<div className="group" onClick={() => handleSetCategory("Main")}>
+						<div
+							className={`group ${activeGroup === "Main" && "select-group"}`}
+							onClick={() => handleSetGroup("Main")}>
 							Main
 						</div>
-						{distinctCategories.map(category => {
+						{distinctCategories.map(group => {
 							return (
 								<div
-									key={category}
-									className="group"
-									onClick={() => handleSetCategory(category)}>
-									{category}
+									key={group}
+									className={`group ${group === activeGroup && "select-group"}`}
+									onClick={() => handleSetGroup(group)}>
+									{group}
 								</div>
 							)
 						})}
