42ab39eb9fac28b0a318ca0dfa219b31319142f3
Baran <baranali6688@gmail.com>
2023-11-20 21:23:11 +0300

    Fixing Auth Page!

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -66,77 +66,79 @@ export default function AuthModal({ onLogin }) {
 	}
 
 	return (
-		<div className={`${error ? " pt-14" : ""} newmodal`}>
-			{error && (
-				<div
-					className="error absolute top-0 w-full left-0 md:left-1/4 md:top-16 md:w-1/2"
-					onClick={() => setError(null)}>
-					{error}
+		<div className="h-screen">
+			<div className={`${error ? " pt-14" : ""} newmodal`}>
+				{error && (
+					<div
+						className="error absolute top-0 w-full left-0 md:left-1/4 md:top-16 md:w-1/2"
+						onClick={() => setError(null)}>
+						{error}
+					</div>
+				)}
+				<div className="flex flex-col">
+					<h1 htmlFor="sign" className="text-2xl mb-4">
+						Sign
+					</h1>
+					<label htmlFor="username">Username</label>
+					<input
+						className="input"
+						type="text"
+						value={username}
+						onChange={e => setUsername(e.target.value)}
+					/>
+					<label htmlFor="password">Password</label>
+					<input
+						className="input"
+						type="password"
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+					/>
+					<a className="text-xs my-2 text-gray-400">Forget Password?</a>
+					<div className="my-4">
+						<button className="btn round-btn text-lg" onClick={() => sign()}>
+							Sign in
+						</button>
+					</div>
 				</div>
-			)}
-			<div className="flex flex-col">
-				<h1 htmlFor="sign" className="text-2xl mb-4">
-					Sign
-				</h1>
-				<label htmlFor="username">Username</label>
-				<input
-					className="input"
-					type="text"
-					value={username}
-					onChange={e => setUsername(e.target.value)}
-				/>
-				<label htmlFor="password">Password</label>
-				<input
-					className="input"
-					type="password"
-					value={password}
-					onChange={e => setPassword(e.target.value)}
-				/>
-				<a className="text-xs my-2 text-gray-400">Forget Password?</a>
-				<div className="my-4">
-					<button className="btn round-btn text-lg" onClick={() => sign()}>
-						Sign in
-					</button>
-				</div>
-			</div>
-			<div className="flex flex-col">
-				<h1 htmlFor="regester" className="text-2xl mb-4">
-					Register
-				</h1>
-				<label htmlFor="name">Name</label>
-				<input
-					type="text"
-					value={newName}
-					onChange={e => setNewName(e.target.value)}
-					className="input"
-				/>
-				<label htmlFor="username">Username</label>
-				<input
-					type="text"
-					value={newUsername}
-					onChange={e => setNewUsername(e.target.value)}
-					className="input"
-				/>
-				<label htmlFor="passowrd">Password</label>
-				<input
-					type="password"
-					value={newPassword}
-					onChange={e => {
-						setNewPassword(e.target.value)
-					}}
-					className="input"
-				/>
-				<label htmlFor="confirmPass">Confirm Password</label>
-				<input
-					type="password"
-					value={confirmPassword}
-					onChange={e => setCofirmPassword(e.target.value)}
-					className="input"
-				/>
-				<div className="my-4">
-					<button className="btn round-btn text-lg" onClick={() => signup()}>
+				<div className="flex flex-col">
+					<h1 htmlFor="regester" className="text-2xl mb-4">
 						Register
-					</button>
+					</h1>
+					<label htmlFor="name">Name</label>
+					<input
+						type="text"
+						value={newName}
+						onChange={e => setNewName(e.target.value)}
+						className="input"
+					/>
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						value={newUsername}
+						onChange={e => setNewUsername(e.target.value)}
+						className="input"
+					/>
+					<label htmlFor="passowrd">Password</label>
+					<input
+						type="password"
+						value={newPassword}
+						onChange={e => {
+							setNewPassword(e.target.value)
+						}}
+						className="input"
+					/>
+					<label htmlFor="confirmPass">Confirm Password</label>
+					<input
+						type="password"
+						value={confirmPassword}
+						onChange={e => setCofirmPassword(e.target.value)}
+						className="input"
+					/>
+					<div className="my-4">
+						<button className="btn round-btn text-lg" onClick={() => signup()}>
+							Register
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
