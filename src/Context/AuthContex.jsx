7d024b540d07df8b506ce20b8a22905b98584979
barham <baranali6688@gmail.com>
2024-02-09 14:25:35 +0300

    API_LINk

--- src/Context/AuthContex.jsx
+++ src/Context/AuthContex.jsx
@@ -1,135 +1,139 @@
-import { useContext, useEffect, useReducer } from "react"
-import { createContext } from "react"
-const states = ["todo", "doing", "done"]
-const apiUrl = "https://tame-ruby-springbok-tam.cyclic.app/"
+import { useContext, useEffect, useReducer } from "react";
+import { createContext } from "react";
+const states = ["todo", "doing", "done"];
+const apiUrl = "https://task-api-zdam.onrender.com/";
 
 const initialState = {
-	user: null,
-	isAuthed: false,
-	token: null,
-	error: "",
-	isLoading: false,
-}
+  user: null,
+  isAuthed: false,
+  token: null,
+  error: "",
+  isLoading: false,
+};
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 function reducer(state, action) {
-	switch (action.type) {
-		case "loading":
-			return { ...state, isLoading: true }
-		case "login":
-			localStorage.setItem(
-				"user",
-				JSON.stringify({
-					user: action.payload.user,
-					token: action.payload.token,
-				})
-			)
-			return {
-				...state,
-				isAuthed: true,
-				user: action.payload.user,
-				token: action.payload.token,
-				error: "",
-				isLoading: false,
-			}
-		case "fail/login":
-			return {
-				...state,
-				error: action.payload,
-				isLoading: false,
-			}
-		case "fail/close":
-			return { ...state, error: "" }
-		case "logout":
-			localStorage.clear("user")
-			return initialState
-	}
+  switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true };
+    case "login":
+      localStorage.setItem(
+        "user",
+        JSON.stringify({
+          user: action.payload.user,
+          token: action.payload.token,
+        })
+      );
+      return {
+        ...state,
+        isAuthed: true,
+        user: action.payload.user,
+        token: action.payload.token,
+        error: "",
+        isLoading: false,
+      };
+    case "fail/login":
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false,
+      };
+    case "fail/close":
+      return { ...state, error: "" };
+    case "logout":
+      localStorage.clear("user");
+      return initialState;
+  }
 }
 
 function AuthProvider({ children }) {
-	const [{ user, isAuthed, token, error }, dispatch] = useReducer(
-		reducer,
-		initialState
-	)
+  const [{ user, isAuthed, token, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
-	const options = {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-	}
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-	useEffect(
-		function () {
-			const savedData = JSON.parse(localStorage.getItem("user")) || null
-			if (savedData)
-				dispatch({
-					type: "login",
-					payload: { user: savedData.user, token: savedData.token },
-				})
-		},
-		[isAuthed]
-	)
+  useEffect(
+    function () {
+      const savedData = JSON.parse(localStorage.getItem("user")) || null;
+      if (savedData)
+        dispatch({
+          type: "login",
+          payload: { user: savedData.user, token: savedData.token },
+        });
+    },
+    [isAuthed]
+  );
 
-	async function login(username, password) {
-		if (!username || !password)
-			return dispatch({ type: "fail/login", payload: "Please fill all fields" })
-		dispatch({ type: "loading" })
+  async function login(username, password) {
+    if (!username || !password)
+      return dispatch({
+        type: "fail/login",
+        payload: "Please fill all fields",
+      });
+    dispatch({ type: "loading" });
 
-		const user = { username, password }
-		const res = await fetch(`${apiUrl}/auth/login`, {
-			...options,
-			body: JSON.stringify(user),
-		})
-		const data = await res.json()
-		if (res.ok) {
-			dispatch({ type: "login", payload: data })
-		}
-		if (!res.ok) {
-			dispatch({ type: "fail/login", payload: data.message })
-		}
-	}
+    const user = { username, password };
+    const res = await fetch(`${apiUrl}/auth/login`, {
+      ...options,
+      body: JSON.stringify(user),
+    });
+    const data = await res.json();
+    if (res.ok) {
+      dispatch({ type: "login", payload: data });
+    }
+    if (!res.ok) {
+      dispatch({ type: "fail/login", payload: data.message });
+    }
+  }
 
-	async function signup(name, username, password, passwordConfirm) {
-		if (!name || !username || !password || !passwordConfirm) {
-			return dispatch({
-				type: "fail/login",
-				payload: "Please fill all fields!",
-			})
-		}
-		if (password !== passwordConfirm) {
-			return dispatch({
-				type: "fail/login",
-				payload: "Password and Confirm Password Should be the same!",
-			})
-		}
-		const user = { name, username, password, passwordConfirm }
-		const res = await fetch(`${apiUrl}/auth/signup`, {
-			...options,
-			body: JSON.stringify(user),
-		})
-		const data = await res.json()
-		if (res.ok) {
-			dispatch({ type: "login", payload: data })
-		}
-		if (!res.ok) {
-			dispatch({ type: "fail/login", payload: data.message })
-		}
-	}
+  async function signup(name, username, password, passwordConfirm) {
+    if (!name || !username || !password || !passwordConfirm) {
+      return dispatch({
+        type: "fail/login",
+        payload: "Please fill all fields!",
+      });
+    }
+    if (password !== passwordConfirm) {
+      return dispatch({
+        type: "fail/login",
+        payload: "Password and Confirm Password Should be the same!",
+      });
+    }
+    const user = { name, username, password, passwordConfirm };
+    const res = await fetch(`${apiUrl}/auth/signup`, {
+      ...options,
+      body: JSON.stringify(user),
+    });
+    const data = await res.json();
+    if (res.ok) {
+      dispatch({ type: "login", payload: data });
+    }
+    if (!res.ok) {
+      dispatch({ type: "fail/login", payload: data.message });
+    }
+  }
 
-	return (
-		<AuthContext.Provider
-			value={{ user, signup, isAuthed, login, token, error, dispatch }}>
-			{children}
-		</AuthContext.Provider>
-	)
+  return (
+    <AuthContext.Provider
+      value={{ user, signup, isAuthed, login, token, error, dispatch }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
-	const context = useContext(AuthContext)
-	if (context === undefined) throw new Error("Used in Wrong Place!")
-	return context
+  const context = useContext(AuthContext);
+  if (context === undefined) throw new Error("Used in Wrong Place!");
+  return context;
 }
 
-export { useAuth, AuthProvider }
+export { useAuth, AuthProvider };
--- src/Context/TasksContext.jsx
+++ src/Context/TasksContext.jsx
@@ -1,170 +1,171 @@
-import { useEffect } from "react"
-import { useContext } from "react"
-import { useState } from "react"
-import { useReducer } from "react"
-import { createContext } from "react"
-import { useAuth } from "./AuthContex"
-const apiUrl = "https://tame-ruby-springbok-tam.cyclic.app/"
+import { useEffect } from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import { useReducer } from "react";
+import { createContext } from "react";
+import { useAuth } from "./AuthContex";
+const apiUrl = "https://task-api-zdam.onrender.com/";
 
-const TaskContext = createContext()
+const TaskContext = createContext();
 const intialState = {
-	selected: null,
-	filteredTasks: [],
-	tasks: [],
-}
+  selected: null,
+  filteredTasks: [],
+  tasks: [],
+};
 
 function reducer(state, action) {
-	console.log(action)
-	switch (action.type) {
-		case "fetch":
-			return { ...state, selected: "Main", tasks: action.payload }
+  console.log(action);
+  switch (action.type) {
+    case "fetch":
+      return { ...state, selected: "Main", tasks: action.payload };
 
-		case "main":
-			return {
-				...state,
-				selected: "Main",
-				// tasks: [...state.tasks],
-				// filteredTasks: [...state.tasks],
-			}
+    case "main":
+      return {
+        ...state,
+        selected: "Main",
+        // tasks: [...state.tasks],
+        // filteredTasks: [...state.tasks],
+      };
 
-		case "filter":
-			return {
-				...state,
-				selected: action.payload,
-				filteredTasks: [
-					...state.tasks.filter(task => task.group === action.payload),
-				],
-			}
+    case "filter":
+      return {
+        ...state,
+        selected: action.payload,
+        filteredTasks: [
+          ...state.tasks.filter((task) => task.group === action.payload),
+        ],
+      };
 
-		case "delete":
-			return {
-				...state,
-				selected: state.filteredTasks.length > 1 ? state.selected : "Main",
-				tasks: [...state.tasks.filter(task => task.id !== action.payload)],
-				filteredTasks: [
-					...state.filteredTasks.filter(task => task.id !== action.payload),
-				],
-			}
+    case "delete":
+      return {
+        ...state,
+        selected: state.filteredTasks.length > 1 ? state.selected : "Main",
+        tasks: [...state.tasks.filter((task) => task.id !== action.payload)],
+        filteredTasks: [
+          ...state.filteredTasks.filter((task) => task.id !== action.payload),
+        ],
+      };
 
-		case "change":
-			return {
-				...state,
-				tasks: [
-					...state.tasks.map(task =>
-						task.id === action.payload.id
-							? { ...task, state: action.payload.state }
-							: task
-					),
-				],
-				filteredTasks: [
-					...state.filteredTasks.map(task =>
-						task.id === action.payload.id
-							? { ...task, state: action.payload.state }
-							: task
-					),
-				],
-			}
+    case "change":
+      return {
+        ...state,
+        tasks: [
+          ...state.tasks.map((task) =>
+            task.id === action.payload.id
+              ? { ...task, state: action.payload.state }
+              : task
+          ),
+        ],
+        filteredTasks: [
+          ...state.filteredTasks.map((task) =>
+            task.id === action.payload.id
+              ? { ...task, state: action.payload.state }
+              : task
+          ),
+        ],
+      };
 
-		case "add":
-			return {
-				...state,
-				tasks: [action.payload, ...state.tasks],
-				filteredTasks: [
-					action.payload.group === state.selected && action.payload,
-					...state.filteredTasks,
-				],
-			}
+    case "add":
+      return {
+        ...state,
+        tasks: [action.payload, ...state.tasks],
+        filteredTasks: [
+          action.payload.group === state.selected && action.payload,
+          ...state.filteredTasks,
+        ],
+      };
 
-		case "main":
-			return { ...state, filteredTasks: state.tasks }
-	}
+    case "main":
+      return { ...state, filteredTasks: state.tasks };
+  }
 }
 
 function TaskProvider({ children }) {
-	const [{ tasks, filteredTasks, selected }, dispatch] = useReducer(
-		reducer,
-		intialState
-	)
-	const [categories, setDistinctCategories] = useState([])
-	const { token, isAuthed } = useAuth()
-	const headers = new Headers({
-		"Content-Type": "application/json",
-		Authorization: `Bearer ${token}`,
-	})
+  const [{ tasks, filteredTasks, selected }, dispatch] = useReducer(
+    reducer,
+    intialState
+  );
+  const [categories, setDistinctCategories] = useState([]);
+  const { token, isAuthed } = useAuth();
+  const headers = new Headers({
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  });
 
-	useEffect(
-		function () {
-			async function fetchTasks() {
-				const res = await fetch(`${apiUrl}/tasks`, { method: "GET", headers })
-				const json = await res.json()
-				dispatch({ type: "fetch", payload: json.data })
-			}
-			if (isAuthed) fetchTasks()
-		},
-		[isAuthed]
-	)
+  useEffect(
+    function () {
+      async function fetchTasks() {
+        const res = await fetch(`${apiUrl}/tasks`, { method: "GET", headers });
+        const json = await res.json();
+        dispatch({ type: "fetch", payload: json.data });
+      }
+      if (isAuthed) fetchTasks();
+    },
+    [isAuthed]
+  );
 
-	useEffect(
-		function () {
-			if (tasks && tasks.length >= 0)
-				setDistinctCategories([...new Set(tasks?.map(task => task.group))])
-		},
-		[tasks]
-	)
+  useEffect(
+    function () {
+      if (tasks && tasks.length >= 0)
+        setDistinctCategories([...new Set(tasks?.map((task) => task.group))]);
+    },
+    [tasks]
+  );
 
-	async function onDelete(id) {
-		dispatch({ type: "delete", payload: id })
-		await fetch(`${apiUrl}/tasks/${id}`, {
-			method: "DELETE",
-			headers,
-		})
-	}
+  async function onDelete(id) {
+    dispatch({ type: "delete", payload: id });
+    await fetch(`${apiUrl}/tasks/${id}`, {
+      method: "DELETE",
+      headers,
+    });
+  }
 
-	async function onChange(id, state) {
-		dispatch({ type: "change", payload: { id, state } })
-		await fetch(`${apiUrl}/tasks/${id}`, {
-			method: "PATCH",
-			headers,
-			body: JSON.stringify({ state }),
-		})
-	}
+  async function onChange(id, state) {
+    dispatch({ type: "change", payload: { id, state } });
+    await fetch(`${apiUrl}/tasks/${id}`, {
+      method: "PATCH",
+      headers,
+      body: JSON.stringify({ state }),
+    });
+  }
 
-	function handleFilter(group) {
-		dispatch({ type: "filter", payload: group })
-	}
+  function handleFilter(group) {
+    dispatch({ type: "filter", payload: group });
+  }
 
-	async function onAdd(newTask) {
-		const res = await fetch(`${apiUrl}/tasks`, {
-			method: "POST",
-			headers,
-			body: JSON.stringify(newTask),
-		})
-		const { data } = await res.json()
-		dispatch({ type: "add", payload: data })
-	}
+  async function onAdd(newTask) {
+    const res = await fetch(`${apiUrl}/tasks`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(newTask),
+    });
+    const { data } = await res.json();
+    dispatch({ type: "add", payload: data });
+  }
 
-	return (
-		<TaskContext.Provider
-			value={{
-				tasks,
-				filteredTasks,
-				selected,
-				categories,
-				handleFilter,
-				onDelete,
-				onChange,
-				onAdd,
-				dispatch,
-			}}>
-			{children}
-		</TaskContext.Provider>
-	)
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        filteredTasks,
+        selected,
+        categories,
+        handleFilter,
+        onDelete,
+        onChange,
+        onAdd,
+        dispatch,
+      }}
+    >
+      {children}
+    </TaskContext.Provider>
+  );
 }
 
 function useTasks() {
-	const context = useContext(TaskContext)
-	if (context === undefined) throw new Error("SOmething Bad")
-	return context
+  const context = useContext(TaskContext);
+  if (context === undefined) throw new Error("SOmething Bad");
+  return context;
 }
 
-export { useTasks, TaskProvider }
+export { useTasks, TaskProvider };
